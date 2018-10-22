0f9315f667154f1d061d0e23273162695b3905e3
Nathan <nathanlacasse@hotmail.com>
2018-10-22 19:25:30 -0400

    2nd Player Movement

---
+++ Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour
+{
+
+    public float velocity;
+
+    private float angularVelocity;
+    private bool corner;
+    private float totalAngle;
+    private Vector3 center;         // Center of rotation circle
+    private float raduis;           // Radius of rotation circle
+    private const string cornerTriggerTag = "CornerTrigger";
+
+    private void Start() {
+        raduis = Mathf.Abs(transform.position.z);
+        angularVelocity = velocity / (2 * raduis);
+        totalAngle = 0;
+        corner = false;
+    }
+
+    private void Update() {
+        float inputHorizontal = Input.GetAxis("Horizontal2");
+        Vector3 pos = transform.position;
+
+        if (!corner)        // Move the character in a straight line
+        {
+            Vector3 movement = transform.forward * velocity * inputHorizontal * Time.deltaTime;
+            pos += movement;
+
+            // Round depth to +radius or -raduis            
+            int sign = (int)Mathf.Round(pos.z / Mathf.Abs(pos.z));
+            pos.z = sign * raduis;
+        }
+        else            // Move the character in a circular motion
+        {
+            totalAngle += -inputHorizontal * angularVelocity * Time.deltaTime;
+            pos.x = center.x + raduis * Mathf.Sin(totalAngle);
+            pos.z = center.z + raduis * Mathf.Cos(totalAngle);
+        }
+
+        transform.position = pos;
+        transform.rotation = Quaternion.AngleAxis(totalAngle * Mathf.Rad2Deg - 90, Vector3.up);
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag.Equals(cornerTriggerTag))     // Entering a corner
+        {
+            corner = true;
+            center = transform.position;
+            center.z = 0;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag.Equals(cornerTriggerTag))
+        {                                           // Exiting a corner
+
+            corner = false;
+
+            // Round rotation to k * PI
+            // Find k
+            float k = Mathf.Round(totalAngle / (Mathf.PI));
+            totalAngle = k * Mathf.PI;
+
+        }
+    }
+
+
+
+}

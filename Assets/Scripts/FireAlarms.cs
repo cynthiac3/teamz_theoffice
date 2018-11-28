ad65f5f85d9ca4f6d83302e716c17089d5684469
MarkZ2018 <41009850+MarkZ2018@users.noreply.github.com>
2018-11-28 02:28:35 -0500

    Adjusted spawn locations for Alarms and Generators

--- Assets/Scripts/FireAlarms.cs
+++ Assets/Scripts/FireAlarms.cs
@@ -15,12 +15,29 @@ public class FireAlarms : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        float yDisplacement = 1.8f;
 
         for (int i = 0; i < 5; i++)
         {
             int alarmPos = Random.Range(0, 5);
-            newAlarms[i] = Instantiate(alarmSet[(i * 5) + alarmPos], transform);
-            circle[i] = Instantiate(useCircle, newAlarms[i].transform.position + new Vector3(0, -1.75f, 0), Quaternion.Euler(-90, 0, 0));
+
+            if (i == 0)
+            {
+                newAlarms[i] = Instantiate(alarmSet[(i * 5) + alarmPos], transform);
+                newAlarms[i].transform.position -= new Vector3(0, yDisplacement, 0);
+                circle[i] = Instantiate(useCircle, newAlarms[i].transform.position + new Vector3(0, -1.75f, 0), Quaternion.Euler(-90, 0, 0));
+            }
+            else if(i % 2 == 0)
+            {
+                newAlarms[i] = Instantiate(alarmSet[(i * 5) + alarmPos], transform);
+                newAlarms[i].transform.position += new Vector3(0, yDisplacement * 3, 0);
+                circle[i] = Instantiate(useCircle, newAlarms[i].transform.position + new Vector3(0, -1.75f, 0), Quaternion.Euler(-90, 0, 0));
+            }
+            else
+            {
+                newAlarms[i] = Instantiate(alarmSet[(i * 5) + alarmPos], transform);
+                circle[i] = Instantiate(useCircle, newAlarms[i].transform.position + new Vector3(0, -1.75f, 0), Quaternion.Euler(-90, 0, 0));
+            }
         }
         GameObject sprinkObj = GameObject.Find("Sprinklers");
         sprinkScript = sprinkObj.GetComponent<Sprinklers>();
